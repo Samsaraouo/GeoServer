221e7ded9ee3290da52556354e5004b79193c04e
Samsaraouo <xmj1175542054@163.com>
2024-03-27 23:07:56 +0800

    changed

--- GeoServer/GeoServer/settings.py
+++ GeoServer/GeoServer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'GeoServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'OMM',
+        'USER': 'postgres',
+        'PASSWORD':'123456',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'
     }
 }
 
@@ -117,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static'), # 添加内容
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- GeoServer/app/models.py
+++ GeoServer/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.gis.db import models as GeoModels
 # Create your models here.
+
+class Users(models.Model):
+    id = models.AutoField('记录编号',primary_key=True)
+     
